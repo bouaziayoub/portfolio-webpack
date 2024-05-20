c9df493c0052f1f1dfbaef173aeba75d4364a006
ayoub bouazi <114018977+bouaziayoub@users.noreply.github.com>
2024-05-21 00:07:10 +0200

    Add contact section

--- src/views/contact/contact.js
+++ src/views/contact/contact.js
@@ -5,38 +5,50 @@ const renderContact = () => {
 
   root.innerHTML += `
     <div class="contact">
+    <div class="contact-header title-section slide-top">
       <h2>Contacto</h2>
+    </div>
+
+    <div class="contact-container slide-top">
+
       <div class="contact-info">
-        <div class="contact-item">
-          <h3>Dirección:</h3>
-          <p>123 Calle Principal, Ciudad, País</p>
-        </div>
-        <div class="contact-item">
-          <h3>Teléfono:</h3>
-          <p>+123 456 7890</p>
-        </div>
-        <div class="contact-item">
-          <h3>Correo Electrónico:</h3>
-          <p>correo@example.com</p>
+          <div class="contact-item">
+            <h3>Dirección:</h3>
+            <p>123 Calle Principal, Ciudad, País</p>
+          </div>
+          <div class="contact-item">
+            <h3>Teléfono:</h3>
+            <p>+123 456 7890</p>
+          </div>
+          <div class="contact-item">
+            <h3>Correo Electrónico:</h3>
+            <p>correo@example.com</p>
+          </div>
+      </div>
+
+
+      <div class="form-message ">
+      <h3 class ="sub-title-section">¿Tienes alguna pregunta o comentario?</h3>
+      <div class="form-message-header">
+        <p>Envíanos un mensaje y te responderemos lo antes posible.</p>
+     
+        <button id="show-form-btn" class="show-form-btn">Enviar Mensaje</button>
+        </div> 
+        <form class="contact-form">
+            <div class="form-group">
+             
+              <input type="text" id="name" name="name" placeholder="Nombre">
+            </div>
+            <div class="form-group">
+              <input type="email" id="email" name="email" placeholder="E-mail">
+            </div>
+            <div class="form-group">
+              <textarea id="message" name="message" rows="4" placeholder="Message"></textarea>
+            </div>
+            <button type="submit">Enviar</button>
+          </form>
         </div>
       </div>
-      <button id="show-form-btn" class="show-form-btn">Enviar Mensaje</button>
-      <form class="contact-form">
-        <h3>Envíanos un mensaje</h3>
-        <div class="form-group">
-          <label for="name">Nombre:</label>
-          <input type="text" id="name" name="name" required>
-        </div>
-        <div class="form-group">
-          <label for="email">Correo Electrónico:</label>
-          <input type="email" id="email" name="email" required>
-        </div>
-        <div class="form-group">
-          <label for="message">Mensaje:</label>
-          <textarea id="message" name="message" rows="4" required></textarea>
-        </div>
-        <button type="submit">Enviar</button>
-      </form>
     </div>
   `;
 
@@ -44,11 +56,10 @@ const renderContact = () => {
     const contactForm = event.target
       .closest(".contact")
       .querySelector(".contact-form");
-    contactForm.classList.toggle("active");
+    contactForm.classList.toggle("active-form");
   };
 
   root.addEventListener("click", (event) => {
-    console.log(event.target);
     if (event.target.matches("#show-form-btn")) {
       toggleForm(event);
     }
