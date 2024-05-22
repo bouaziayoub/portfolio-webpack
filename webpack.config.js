7f7f2089b5653817a986ef8cab2d4e5d099627f6
ayoub bouazi <114018977+bouaziayoub@users.noreply.github.com>
2024-05-22 04:24:19 +0200

    Baach to last version of webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,47 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin'); // Importa el plugin
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Plugin para generar el archivo HTML
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
+  const isProduction = argv.mode === 'production'; // Variable para saber si estamos en producción o desarrollo
 
   return {
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+    entry: './src/index.js', // Archivo de entrada
+    output: { 
+      filename: 'bundle.js', // Archivo de salida
+      path: path.resolve(__dirname, 'dist'), // Carpeta de salida
       clean: true,
     },
     mode: isProduction ? 'production' : 'development',
     module: {
       rules: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          test: /\.css$/, 
+          use: ['style-loader', 'css-loader'], // Para cargar archivos CSS
         },
         {
-          test: /\.html$/,
-          use: ['html-loader'],
+          test: /\.html$/, // Para cargar archivos HTML
+          use: ['html-loader'], // Para cargar imágenes en el archivo HTML
         },
         {
-          test: /\.(png|jpe?g|gif|svg)$/i,
+          test: /\.(png|jpe?g|gif|svg)$/i, // Para manejar archivos de imagen
           type: 'asset/resource',
         },
       ],
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-          { from: 'src/assets/json', to: 'assets/json' }, // Copia los archivos JSON a 'dist/assets/json'
-        ],
+        template: './src/index.html', // Plantilla HTML
       }),
     ],
     devServer: {
       static: {
-        directory: path.resolve(__dirname, 'dist'),
+        directory: path.resolve(__dirname, 'dist'), // Para que el servidor de desarrollo sirva los archivos desde la carpeta dist
       },
-      compress: true,
+      compress: true, // Para que los archivos se sirvan comprimidos
       port: 9000,
       open: true,
-      historyApiFallback: {
-        index: 'index.html',
+      historyApiFallback: { // Para que funcione el router en el servidor de desarrollo
+        index: 'index.html'
       },
     },
   };
